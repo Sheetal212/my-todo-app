c06e38dd59e2210ea7cf96e94c8600a097f0fa4c
Sheetal <more8672@gmail.com>
2023-04-14 16:15:22 -0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,10 @@
+FILEPATH= 'todo.txt'
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, "r") as file:  # with context manager method
+        todos_local = file.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
